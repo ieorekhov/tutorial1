69c7094e2d8b681d0371ac30d634cc4971997641
ieorekhov <ieorekhov@gmail.com>
2022-02-01 15:54:33 +0300

    final update

--- tutorial1.cpp
+++ tutorial1.cpp
@@ -5,7 +5,7 @@
 int sum(std::vector<int>& vec)
 {
 	int sum = 0;
-	for (int& elem : vec)
+	for (auto elem : vec) // type deducing
 		sum += elem;
 	return sum;
 }
@@ -14,5 +14,5 @@ int main()
 {
 	std::vector<int> vec = {1,2,3};
 	int summ = sum(vec);
-	std::cout << summ;
+	std::cout << vec.at(1) << summ;
 }
